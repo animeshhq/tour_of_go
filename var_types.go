6b990fe3c881e6b5ac3ea6ae8eb4d0b81df938f0
animeshhq <animeshhq@gmail.com>
2025-09-20 23:22:44 +0530

    modified:   var_types.go

--- var_types.go
+++ var_types.go
@@ -1,3 +1,11 @@
+/*
+var types in go :
+basic - number, string, bool etc
+aggregate - array, struct etc
+reference - pointers, slices, functions, channels, maps etc
+interfaces
+*/
+
 package main
 
 import (
@@ -5,7 +13,7 @@ import (
 	"math/cmplx"
 )
 
-var python, js, golang bool
+var python, js, golang bool // we need to use var when it comes to writing package level variables
 
 var (
 	ToBe   bool       = false
@@ -14,7 +22,7 @@ var (
 )
 
 func main() {
-	i := 10
+	i := 10 //this is same as writing var i int = 10, but since we are in a function level, we can use this ":=" as a syntatic sugar
 	fmt.Printf("python: Type: %T Value: %v | js: Type: %T Value: %v | golang: Type: %T Value: %v\n", python, python, js, js, golang, golang)
 	fmt.Printf("Type: %T Value: %v\n", i, i)
 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
